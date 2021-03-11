c39478e5fa3ffc8c0aa6898d4f0f2fdcab1c5f0b
xnuinside@gmail.com <xnuinside@gmail.com>
2021-03-11 15:18:39 +0300

    added more tests

--- tests/test_alter_statements.py
+++ tests/test_alter_statements.py
@@ -128,7 +128,7 @@ def test_alter_table_initial_support():
                 "columns": [
                     {
                         "name": "material_id",
-                        'constraint_name': None,
+                        "constraint_name": None,
                         "references": {
                             "column": "id",
                             "table": "materials",
@@ -137,7 +137,7 @@ def test_alter_table_initial_support():
                     },
                     {
                         "name": "attachment_id",
-                        'constraint_name': None,
+                        "constraint_name": None,
                         "references": {
                             "column": "id",
                             "table": "attachments",
@@ -606,7 +606,7 @@ def test_alter_foreiggn_with_constraint():
                 "columns": [
                     {
                         "name": "id",
-                        "constraint_name": 'fk_group',
+                        "constraint_name": "fk_group",
                         "references": {
                             "column": "id",
                             "table": "employees",
@@ -626,3 +626,272 @@ def test_alter_foreiggn_with_constraint():
         }
     ]
     assert parse_results == expected
+
+
+def test_alter_without_constraint_and_constraint_in_table():
+    parse_results = DDLParser(
+        """
+        CREATE TABLE Persons (
+        ID int NOT NULL,
+        LastName varchar(255) NOT NULL,
+        FirstName varchar(255),
+        Age int,
+        City varchar(255),
+        CONSTRAINT CHK_Person CHECK (Age>=18 AND City='Sandnes')
+        );
+
+        ALTER TABLE Persons ADD CHECK (Age>=18 AND City='Sandnes');
+
+        """
+    ).run()
+    expected = [
+        {
+            "columns": [
+                {
+                    "name": "ID",
+                    "type": "int",
+                    "size": None,
+                    "references": None,
+                    "unique": False,
+                    "nullable": False,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "LastName",
+                    "type": "varchar",
+                    "size": 255,
+                    "references": None,
+                    "unique": False,
+                    "nullable": False,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "FirstName",
+                    "type": "varchar",
+                    "size": 255,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "Age",
+                    "type": "int",
+                    "size": None,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "City",
+                    "type": "varchar",
+                    "size": 255,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": None,
+                },
+            ],
+            "primary_key": [],
+            "alter": {
+                "checks": [
+                    {
+                        "constraint_name": None,
+                        "statement": ["Age>=18", "AND", "City='Sandnes'"],
+                    }
+                ]
+            },
+            "checks": [
+                {
+                    "constraint_name": "CHK_Person",
+                    "statement": "Age>=18 AND City='Sandnes'",
+                }
+            ],
+            "table_name": "Persons",
+            "schema": None,
+        }
+    ]
+    assert expected == parse_results
+
+
+def test_combo_with_alter_and_table_constraints():
+    parse_results = DDLParser(
+        """
+    CREATE TABLE employees (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR (50),
+        last_name VARCHAR (50),
+        birth_date DATE CHECK (birth_date > '1900-01-01'),
+        joined_date DATE CHECK (joined_date > birth_date),
+        salary numeric CHECK(salary > 0)
+    );
+    CREATE TABLE Persons (
+        ID int NOT NULL,
+        LastName varchar(255) NOT NULL,
+        FirstName varchar(255),
+        Age int,
+        City varchar(255),
+        CONSTRAINT CHK_Person CHECK (Age>=18 AND City='Sandnes')
+    );
+    
+    ALTER TABLE Persons ADD CHECK (Age>=18 AND City='Sandnes);
+    
+    ALTER TABLE Persons Add CONSTRAINT ck_person  CHECK (Age>=18 AND City='Sandnes);
+    Alter Table Persons ADD CONSTRAINT fk_group FOREIGN KEY (id) REFERENCES employees (id); 
+        """
+    ).run()
+
+    expected = [
+        {
+            "columns": [
+                {
+                    "name": "id",
+                    "type": "SERIAL",
+                    "size": None,
+                    "references": None,
+                    "unique": False,
+                    "nullable": False,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "first_name",
+                    "type": "VARCHAR",
+                    "size": 50,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "last_name",
+                    "type": "VARCHAR",
+                    "size": 50,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "birth_date",
+                    "type": "DATE",
+                    "size": None,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": "birth_date > '1900-01-01'",
+                },
+                {
+                    "name": "joined_date",
+                    "type": "DATE",
+                    "size": None,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": "joined_date > birth_date",
+                },
+            ],
+            "primary_key": ["id"],
+            "alter": {},
+            "checks": [],
+            "table_name": "employees",
+            "schema": None,
+        },
+        {
+            "columns": [
+                {
+                    "name": "ID",
+                    "type": "int",
+                    "size": None,
+                    "references": None,
+                    "unique": False,
+                    "nullable": False,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "LastName",
+                    "type": "varchar",
+                    "size": 255,
+                    "references": None,
+                    "unique": False,
+                    "nullable": False,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "FirstName",
+                    "type": "varchar",
+                    "size": 255,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "Age",
+                    "type": "int",
+                    "size": None,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": None,
+                },
+                {
+                    "name": "City",
+                    "type": "varchar",
+                    "size": 255,
+                    "references": None,
+                    "unique": False,
+                    "nullable": True,
+                    "default": None,
+                    "check": None,
+                },
+            ],
+            "primary_key": [],
+            "alter": {
+                "checks": [
+                    {
+                        "constraint_name": None,
+                        "statement": ["Age>=18", "AND", "City='Sandnes"],
+                    },
+                    {
+                        "constraint_name": "ck_person",
+                        "statement": ["Age>=18", "AND", "City='Sandnes"],
+                    },
+                ],
+                "columns": [
+                    {
+                        "name": "id",
+                        "constraint_name": "fk_group",
+                        "references": {
+                            "column": "id",
+                            "table": "employees",
+                            "schema": None,
+                        },
+                    }
+                ],
+            },
+            "checks": [
+                {
+                    "constraint_name": "CHK_Person",
+                    "statement": "Age>=18 AND City='Sandnes'",
+                }
+            ],
+            "table_name": "Persons",
+            "schema": None,
+        },
+    ]
+    assert expected == parse_results
