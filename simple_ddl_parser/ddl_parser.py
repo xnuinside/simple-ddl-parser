c5b6b06e51cd2eaf7daaef8149da874ad0161f14
jordan <slurpyb@protonmail.com>
2023-07-16 14:20:51 +0800

    Move inline flag to start of regex
    3.11 requires global flags at the start (bpo-47066). See https://github.com/python/cpython/issues/91222

--- simple_ddl_parser/ddl_parser.py
+++ simple_ddl_parser/ddl_parser.py
@@ -153,7 +153,7 @@ class DDLParser(
         return False
 
     def t_AUTOINCREMENT(self, t: LexToken):
-        r"(AUTO_INCREMENT|AUTOINCREMENT)(?i)\b"
+        r"(?i:AUTO_INCREMENT|AUTOINCREMENT)\b"
         t.type = "AUTOINCREMENT"
         return self.set_last_token(t)
 
