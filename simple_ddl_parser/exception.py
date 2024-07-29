e2871fe4035fc45e8e7bc2726d4ba558e36199f8
demitryfly <erropp@ya.ru>
2024-07-29 21:52:42 +0300

    fix DDLParserError for backward compatibility

--- simple_ddl_parser/exception.py
+++ simple_ddl_parser/exception.py
@@ -6,3 +6,8 @@ __all__ = [
 class SimpleDDLParserException(Exception):
     """ Base exception in simple ddl parser library """
     pass
+
+
+class DDLParserError(SimpleDDLParserException):
+    """ An alias for backward compatibility """
+    pass
